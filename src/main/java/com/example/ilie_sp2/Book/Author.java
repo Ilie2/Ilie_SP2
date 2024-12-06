be65efb3186262ad0c173fd5fcfe43e6f199f200
Ilie2 <147348536+Ilie2@users.noreply.github.com>
2024-12-06 09:31:18 +0200

    am facut lab 5-6

--- src/main/java/com/example/ilie_sp2/Book/Author.java
+++ src/main/java/com/example/ilie_sp2/Book/Author.java
@@ -1,14 +1,25 @@
 package com.example.ilie_sp2.Book;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String surname;
 
-    public Author(String name) {
+    public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
-    public void print(){
+
+    public void print() {
         System.out.println(name + " " + surname);
     }
 }
--- src/main/java/com/example/ilie_sp2/Book/Book.java
+++ src/main/java/com/example/ilie_sp2/Book/Book.java
@@ -1,42 +1,54 @@
 package com.example.ilie_sp2.Book;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String title;
-    private List<Element> elements=new ArrayList<>();
-    private List<Author> authors=new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<BaseElement> elements = new ArrayList<>();
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "book_authors",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private List<Author> authors = new ArrayList<>();
+
     public Book(String title) {
-        this.title=title;
+        this.title = title;
     }
-    public void addElement(Element element) {
+
+    public void addElement(BaseElement element) {
         elements.add(element);
     }
+
     public void addAuthor(Author author) {
         authors.add(author);
     }
+
     public void print() {
-        System.out.println("Title: "+title);
+        System.out.println("Title: " + title);
         System.out.println("Authors: ");
         for (Author author : authors) {
             author.print();
         }
-        System.out.println("Content: "+elements);
+        System.out.println("Content: ");
         for (Element element : elements) {
             element.print();
         }
-    }
-
-    public void addContent(Paragraph paragraph) {
-        paragraph.print();
-    }
-
-    public void addContent(Section cap1) {
-        cap1.print();
-    }
-
-    public Object getTitle() {
-        return title;
     }
 }

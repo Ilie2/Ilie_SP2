8bacdbdbaa3cfc83f5573c74b24e3e1d6829f601
Ilie2 <147348536+Ilie2@users.noreply.github.com>
2025-01-17 10:01:39 +0200

    schimbari lab 7

--- src/main/java/com/example/ilie_sp2/repos/AuthorRepos.java
+++ src/main/java/com/example/ilie_sp2/repos/AuthorRepos.java
@@ -1,7 +1,5 @@
 package com.example.ilie_sp2.repos;
-
 import com.example.ilie_sp2.Book.Author;
-import com.example.ilie_sp2.Book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
